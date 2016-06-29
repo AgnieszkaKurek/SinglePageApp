d60e4ab53a858b0d5ad0a0433224fed4ab674f84
Agnieszka Kurek <agniechakurek@gmail.com>
2016-06-29 10:16:33 +0200

    remove unused files, streamlining operations

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-window.addEventListener('hashchange', function() {
-    if (window.location.hash === '#/bookmark/1') {
-        console.log('Page 1 is cool.');
-    }
-    if (window.location.hash === '#/bookmark/2') {
-        console.log('Let me go get Page 2.');
-    }
-    if (window.location.hash === '#/bookmark/3') {
-        console.log('Here is Page 3.');
-    }
+var myApp = angular.module('myApp', [ 'ngRoute']);
+myApp.config(function ($routeProvider) {
+ $routeProvider
+ 
+ .when('/',{
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+ })  
+ .when('/second', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+     
+ })
 });
+myApp.controller('mainController', ['$scope', '$log',
+function($scope,  $log) {
+    $scope.name = 'Main';
+}]);
+myApp.controller('secondController', ['$scope', '$log',
+function($scope,  $log) {
+    $scope.name = 'Second';
+}]);
