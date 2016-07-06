678640b7a3d4aafd417542719367cecfd54036df
Agnieszka Kurek <agniechakurek@gmail.com>
2016-07-06 18:03:53 +0200

    improve the operation of ruting

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-var myApp = angular.module('myApp', [ 'ngRoute']);
-myApp.config(function ($routeProvider) {
- $routeProvider
- 
- .when('/',{
-    templateUrl: 'pages/main.html',
-    controller: 'mainController'
- })  
- .when('/second', {
-    templateUrl: 'pages/second.html',
-    controller: 'secondController'
-     
+'use strict';
+
+var eventsApp = angular.module('eventsApp', ['ngResource'])
+.config(function($routeProvider) {
+ $routeProvider.when('/Home',
+ {
+     templateUrl:'templates/Home.html',
+     controller: 'EditEventController'
  })
 });
-myApp.controller('mainController', ['$scope', '$log',
-function($scope,  $log) {
-    $scope.name = 'Main';
-}]);
-myApp.controller('secondController', ['$scope', '$log',
-function($scope,  $log) {
-    $scope.name = 'Second';
-}]);
+  
