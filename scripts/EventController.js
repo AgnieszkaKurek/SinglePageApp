bdc14688b28e3ce9226fa1f197d6706688d368a1
Agnieszka Kurek <agniechakurek@gmail.com>
2016-08-26 17:31:51 +0200

    add new event

--- scripts/EventController.js
+++ scripts/EventController.js
@@ -5,9 +5,13 @@ eventsApp.controller('EventController',
 function EventController($scope) {
     
     $scope.event = {
-        name: 'Pig Lover is here',
-        date: '24/11/2016',
-        time: '08:00 pm' 
+        name: 'Prosius Birthday',
+        date: '01/02/2017',
+        time: '08:00 pm',
+    location: {
+        address: 'Czeremchowa',
+        city:'Lublin'
+    }
     }
 
 }
