82b7c8deb58aaf7de0045acf2288e830f05b579d
Agnieszka Kurek <agniechakurek@gmail.com>
2016-09-07 16:09:41 +0200

    removing unnecessary files

--- scripts/EventspageController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-eventsApp.controller('EventspageController',
-    function EventspageController($scope,$location, eventData) {
-        $scope.events = eventData.getAllEvents();
-    }
-);
