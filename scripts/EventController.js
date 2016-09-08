6f56593043fabc4d5adf9625552093d437e916ad
Agnieszka Kurek <agniechakurek@gmail.com>
2016-09-08 19:20:42 +0200

    add new event

--- scripts/EventController.js
+++ scripts/EventController.js
@@ -1,17 +1,35 @@
 'use strict';
 
 eventsApp.controller('EventController',
-   
 function EventController($scope) {
+    
     $scope.event = {
-        name: 'Prosius Birthday',
+        name: 'Pig Party',
         date: '01/02/2017',
         time: '08:00 pm',
     location: {
         address: 'Czeremchowa',
         city:'Lublin'
     },
-    imageUrl: '../images/pig.jpg'
+    imageUrl: '../images/pig.jpg',
+    sessions: [
+        {
+            name: 'Prosius Birthday',
+            creatorName:'Ryjek',
+            duration:'4h',
+            abstract:'Fun at the cake and other delicacies with friends and family.',
+            upVoteCount: 0
+        },
+        {
+            name: 'Fun for pigs',
+            creatorName:'Ryjek',
+            duration:'1h',
+            abstract:'Dancing party for large and small  piggies.You can twist your ham.',
+            upVoteCount: 0
+        },
+        
+           
+    ]
     }
 }
 );
